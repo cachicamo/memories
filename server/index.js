71e3accf44cf1b06e239244d500afd86716de517
cachicamo <35459697+cachicamo@users.noreply.github.com>
2021-07-16 12:36:30 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`)))
+  .catch((err) => console.log(err.message));
+
+mongoose.set('useFindAndModify', false);
